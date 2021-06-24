f817bdb8b23cb6bbf57f0e9e79470c3b8b68353f
stephenharrity <sjh401@gmail.com>
2021-06-24 10:08:03 -0500

    added all ingredients

--- app.js
+++ app.js
@@ -90,11 +90,39 @@ const grabDrinkData = async (drinkID) => {
         const drinkFlex = document.getElementById(`${id}`)
         // const drinkFlex = document.
         let strIngredient1 = `${drinkData.strMeasure1} of ${drinkData.strIngredient1}`
+        let strIngredient2 = `, ${drinkData.strMeasure2} of ${drinkData.strIngredient2}`
+        let strIngredient3 = `, ${drinkData.strMeasure3} of ${drinkData.strIngredient3}`
+        let strIngredient4 = `, ${drinkData.strMeasure4} of ${drinkData.strIngredient4}`
+        let strIngredient5 = `, ${drinkData.strMeasure5} of ${drinkData.strIngredient5}`
+        let strIngredient6 = `, ${drinkData.strMeasure6} of ${drinkData.strIngredient6}`
+        let strIngredient7 = `, ${drinkData.strMeasure7} of ${drinkData.strIngredient7}`
+        let strIngredient8 = `, ${drinkData.strMeasure8} of ${drinkData.strIngredient8}`
+        let strIngredient9 = `, ${drinkData.strMeasure9} of ${drinkData.strIngredient9}`
+        let strIngredient10 = `, ${drinkData.strMeasure10} of ${drinkData.strIngredient10}`
+        let strIngredient11 = `, ${drinkData.strMeasure11} of ${drinkData.strIngredient11}`
+        let strIngredient12 = `, ${drinkData.strMeasure12} of ${drinkData.strIngredient12}`
+        let strIngredient13 = `, ${drinkData.strMeasure13} of ${drinkData.strIngredient13}`
+        let strIngredient14 = `, ${drinkData.strMeasure14} of ${drinkData.strIngredient14}`
+        let strIngredient15 = `, ${drinkData.strMeasure15} of ${drinkData.strIngredient15}`
         const drinkIngredients = document.createElement('div')
         drinkIngredients.setAttribute('class', 'ingredient-container')
         drinkIngredients.setAttribute('style','display: flex;')
         drinkFlex.append(drinkIngredients)
-        let strIngredient = (strIngredient1 !== null) ? drinkIngredients.append(strIngredient1): null
+        let str1Ingredient = (drinkData.strIngredient1 !== null) ? drinkIngredients.append(strIngredient1): ''
+        let str2Ingredient = (drinkData.strIngredient2 !== null) ? drinkIngredients.append(strIngredient2): ''
+        let str3Ingredient = (drinkData.strIngredient3 !== null) ? drinkIngredients.append(strIngredient3): ''
+        let str4Ingredient = (drinkData.strIngredient4 !== null) ? drinkIngredients.append(strIngredient4): ''
+        let str5Ingredient = (drinkData.strIngredient5 !== null) ? drinkIngredients.append(strIngredient5): ''
+        let str6Ingredient = (drinkData.strIngredient6 !== null) ? drinkIngredients.append(strIngredient6): ''
+        let str7Ingredient = (drinkData.strIngredient7 !== null) ? drinkIngredients.append(strIngredient7): ''
+        let str8Ingredient = (drinkData.strIngredient8 !== null) ? drinkIngredients.append(strIngredient8): ''
+        let str9Ingredient = (drinkData.strIngredient9 !== null) ? drinkIngredients.append(strIngredient9): ''
+        let str10Ingredient = (drinkData.strIngredient10 !== null) ? drinkIngredients.append(strIngredient10): ''
+        let str11Ingredient = (drinkData.strIngredient11 !== null) ? drinkIngredients.append(strIngredient11): ''
+        let str12Ingredient = (drinkData.strIngredient12 !== null) ? drinkIngredients.append(strIngredient12): ''
+        let str13Ingredient = (drinkData.strIngredient13 !== null) ? drinkIngredients.append(strIngredient13): ''
+        let str14Ingredient = (drinkData.strIngredient14 !== null) ? drinkIngredients.append(strIngredient14): ''
+        let str15Ingredient = (drinkData.strIngredient15 !== null) ? drinkIngredients.append(strIngredient15): ''
         // let i = 1
         // while(i < 3) {
         //     let ingredient = drinkData.strIngredient1
@@ -108,12 +136,3 @@ const grabDrinkData = async (drinkID) => {
     }
 }
 
-// const appendIngredients = (ingredient) => {
-//     const drinkName = document.getElementById(`${id}`)
-//     const drinkIngredients = document.createElement('div')
-//     drinkIngredients.setAttribute('class', 'ingredient-container')
-//     drinkIngredients.setAttribute('style','display: flex;')
-//     let stringredient = ingredient
-//     drinkIngredients.innerText = drinkData.stringredient
-//     drinkName.append(drinkIngredients)
-// }
